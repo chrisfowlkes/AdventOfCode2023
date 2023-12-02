58e413cad8367234f3abb3109b4c979917e2f6e4
cfowlkes <chrisfowlkes@gmail.com>
2023-12-01 19:13:35 -0500

    Day 1 Puzzle 1

--- AoC2023/AoC2023/Program.cs
+++ AoC2023/AoC2023/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Classes;
+
+var input = Console.ReadLine();
+string result;
+switch (input)
+{
+    case "1A":
+        var data = File.ReadAllLines(".\\Data\\1A.txt");
+        var doc = new CalibrationDocument(data);
+        result = doc.Calculate();
+        break;
+    default:
+        result = "Error";
+        break;
+}
+Console.WriteLine(result);
+Console.ReadLine();
---
+++ AoC2023/Tests/CalibrationDocumentTests.cs
@@ -0,0 +1,29 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    /// <summary>
+    /// Tests for the calibration document class.
+    /// </summary>
+    public class CalibrationDocumentTests
+    {
+        [Fact]
+        public void Calculate_DontTranslate_Calculates()
+        {
+            //Arrange
+            var data = File.ReadAllLines(".\\Data\\1A.txt");
+            var c = new CalibrationDocument(data);
+
+            //Act
+            var i = c.Calculate();
+
+            //Assert
+            Assert.Equal("142", i);
+        }
+    }
+}
